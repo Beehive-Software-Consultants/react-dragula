0adb5c19ad4a664ff3027cbf4bb40f980a39478d
Brandon Trabon <brandon.trabon@gmail.com>
2021-07-31 22:19:14 +1000

    Finished project setup.

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import gzipPlugin from 'rollup-plugin-gzip';
+
+const packageJson = require('./package.json');
+
+const config = [
+  {
+    input: 'src/main.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      terser(),
+      gzipPlugin(),
+    ],
+  },
+];
+
+export default config;
